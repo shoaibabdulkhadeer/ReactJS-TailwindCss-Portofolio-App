c79694bf4f3fbefd1fa855f00b3ee9ae2227a5c6
SHOAIB (ZEN) <shoaibabdulkhadeer@gmail.com>
2022-11-15 14:33:25 +0530

    add movie project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import realstate from "../../src/assets/projects/realestate.jpg";
 import crypto from "../../src/assets/projects/crypto.png";
 import Typewriter from "typewriter-effect";
+import movie from "../../src/assets/projects/movie.png"
+import { GrBitcoin } from 'react-icons/gr';
 
 
 const Work = () => {
@@ -37,7 +39,7 @@ const Work = () => {
    <div className="m-3 p-2 flex flex-col items-center justify-center shadow-md shadow-[#040c16] scale-95 hover:scale-100  hover:bg-slate-800 duration-150 cursor-pointer ">  
                <img src={crypto} alt="" className="h-120"></img>
 
-               <p className="m-2 p-2 font-bold">Crypto API Project ✌</p>
+               <p className="m-2 p-2 font-bold flex items-center gap-2"> <GrBitcoin size={25}/> Crypto API Project <GrBitcoin size={25} /> </p>
                
                <p className="p-2 font-bold text-center">React js ,Tailwind css , CoingeckoAPI</p>
                
@@ -77,13 +79,16 @@ const Work = () => {
 
    {/* 2card  */}
    <div className="m-3 p-2 flex flex-col items-center justify-center shadow-md shadow-[#040c16] scale-95 hover:scale-100  hover:bg-slate-800 duration-150 cursor-pointer ">  
-               <img src={realstate} alt="" className="h-120"></img>
+               <img src={movie} alt="" className="h-120"></img>
+               <p className="m-2 p-2 font-bold">🎥 Movie App 🎥</p>
+               
+               <p className="p-2 font-bold text-center">OMDb API ,React js ,Tailwind css </p>
                       
                       {/* buttons */}
                 <div className="flex items-center justify-center mt-2 ">
 
 
-                 <a href="/">                        
+                 <a href="https://github.com/shoaibabdulkhadeer/movie-app">                        
                   <button type="button" class="text-white bg-gradient-to-r mt-2
                   from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg
                    shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80
@@ -98,7 +103,7 @@ const Work = () => {
                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
                        </svg>
 
-                     <a href="/"> WATCH LIVE... </a>  
+                     <a href="https://shoaibmovieapp.netlify.app/"> WATCH LIVE... </a>  
                   </button>
                   </a>
                 </div>
