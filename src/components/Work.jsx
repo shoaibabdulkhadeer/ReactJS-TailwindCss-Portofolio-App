bc128e49cf6836223d545eeacd9c02389c2c17c9
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-03-22 16:47:09 +0530

    Work project update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,6 +4,8 @@ import Typewriter from "typewriter-effect";
 import movie from "../../src/assets/projects/movie.png"
 import { GrBitcoin } from 'react-icons/gr';
 import netf from "../../src/assets/projects/netf.png"
+import twitter from "../assets/projects/twitter.png"
+import { ImTwitter } from 'react-icons/im';
 
 
 const Work = ({theme}) => {
@@ -105,7 +107,7 @@ const Work = ({theme}) => {
                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
                        </svg>
 
-                     <a href="https://github.com/shoaibabdulkhadeer/netlixlap.git"> WATCH LIVE... </a>  
+                     <a href="https://github.com/shoaibabdulkhadeer/netlixlap.git" > WATCH LIVE... </a>  
                   </button>
                   </a>
                 </div>
@@ -118,7 +120,7 @@ const Work = ({theme}) => {
    <div className= {theme? "m-3 p-2 flex flex-col text-black items-center justify-center shadow-md shadow-[#040c16] scale-95 hover:scale-100 hover:text-white  hover:bg-slate-800 duration-150 cursor-pointer "
    : "m-3 p-2 flex flex-col  items-center justify-center shadow-md shadow-[#040c16] scale-95 hover:scale-100  hover:bg-slate-800 duration-150 cursor-pointer "}>  
                <img src={movie} alt="" className="h-120"></img>
-               <p className="m-2 p-2 font-bold">🎥 Movie App 🎥</p>
+               <p className="m-2 p-2 font-bold">🎥 Movie App - Search Any Movie 🎥</p>
                
                <p className="p-2 font-bold text-center">OMDb API ,React js ,Tailwind css </p>
                       
@@ -154,13 +156,16 @@ const Work = ({theme}) => {
    {/* 4card  */}
    <div className= {theme? "m-3 p-2 flex flex-col text-black items-center justify-center shadow-md shadow-[#040c16] scale-95 hover:scale-100 hover:text-white  hover:bg-slate-800 duration-150 cursor-pointer "
    : "m-3 p-2 flex flex-col  items-center justify-center shadow-md shadow-[#040c16] scale-95 hover:scale-100  hover:bg-slate-800 duration-150 cursor-pointer "}>  
-               <img src='/images/const.jpg' alt="" className="h-120"></img>
+               <img src={twitter} alt="" className="h-120"></img>
+                     <p className="m-2 p-2 font-bold flex items-center gap-2"> <ImTwitter size={30}/> Twitter Clone <ImTwitter size={30}/> </p>
+               
+               <p className="p-2 font-bold text-center">Small Project Only FrontEnd - CSS </p>
                       
                       {/* buttons */}
                 <div className="flex items-center justify-center mt-2 ">
 
 
-                 <a href="/">                        
+                 <a href="https://github.com/shoaibabdulkhadeer/react-twitterclone.git">                        
                   <button type="button" class="text-white bg-gradient-to-r mt-2
                   from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg
                    shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80
@@ -175,7 +180,7 @@ const Work = ({theme}) => {
                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
                        </svg>
 
-                     <a href="/"> WATCH LIVE... </a>  
+                     <a href="https://github.com/shoaibabdulkhadeer/react-twitterclone.git"> WATCH LIVE... </a>  
                   </button>
                   </a>
                 </div>
