b5df16370fc04a68875ed0587bc8572b8dc2879a
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-05-17 01:00:35 +0530

    updated work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -205,7 +205,7 @@ const Work = ({theme}) => {
                 <div className="flex items-center justify-center mt-2 ">
 
 
-                 <a href="/">                        
+                 <a href="https://github.com/shoaibabdulkhadeer/ReactJs-Library-management-system">                        
                   <button type="button" class="text-white bg-gradient-to-r mt-2
                   from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg
                    shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80
@@ -213,7 +213,7 @@ const Work = ({theme}) => {
                     >VIEW CODE</button> 
                     </a> 
 
-                  <a href="https://www.google.com/">
+                  <a href="https://shoaiblmsproject.netlify.app/">
                     <button disabled type="button" class=" cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center">
                       <svg aria-hidden="true" role="status" class="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
